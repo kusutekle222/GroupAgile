167301355506865bf0c8fe8f1d68d645c374d776
Kisanet Tesfamichael <kisanet.tesfamichael@mycit.ie>
2020-11-29 08:21:43 +0000

    delete some code

--- HelloPython.py
+++ HelloPython.py
@@ -1,9 +1,5 @@
 import datetime
 print("Hello world!")
-print("Group Red Core")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
-
-print("Hello world!")
-print("Group Red Core")
