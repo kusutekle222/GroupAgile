21fdf7ff72fdd66ae9bb273f9133e85026eea41c
Kisanet Tesfamichael <kisanet.tesfamichael@mycit.ie>
2020-11-30 02:56:30 +0000

    Add code to the file

--- HelloPython.py
+++ HelloPython.py
@@ -1,5 +1,7 @@
 import datetime
 print("Hello world!")
+print("This is core red group")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
+print("This is core red group")
