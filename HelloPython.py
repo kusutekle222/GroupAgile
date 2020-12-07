f571b78dd1caf3a6805ee11d96c2c8828d142a8c
Kisanet Tesfamichael <kisanet.tesfamichael@mycit.ie>
2020-12-07 14:30:47 +0000

    error

--- HelloPython.py
+++ HelloPython.py
@@ -3,5 +3,5 @@ print("Hello world!")
 print("This is core red group")
 now = datetime.datetime.now()
 print ("Current date and time is ")
-print (now.strftime("%A, %d-%m-%Y : %H:%M"))
+print (now.strftime("%A, %d-%m-%Y : %H:%M
 print("This is core red group")
